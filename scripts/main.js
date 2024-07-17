e0b202a5c6695a52af25ecfa78d4a510b85962a7
Will V <43239509+rhit-valentwa@users.noreply.github.com>
2024-07-16 17:25:52 -0700

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,3 @@
-console.log("Thanks for checking out my website! - Will")
+console.log("Thanks for checking out my website!")
+console.log("Here's a link to some of the games I made as a kid - Will")
+console.log("https://will-valentine.com/arcade")
